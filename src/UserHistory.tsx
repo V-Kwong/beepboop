90398e6926d848fb02e0f799228d5786c5458b91
V-Kwong <vincent_kwong578@hotmail.com>
2023-05-09 19:08:50 -0700

    changed timer to show minutes, removed hour

--- src/UserHistory.tsx
+++ src/UserHistory.tsx
@@ -70,16 +70,16 @@ const NINTY_MIN = 90 * 60;
 
 function fancyTimeFormat(duration: number) {
   // Hours, minutes and seconds
-  var hrs = ~~(duration / 3600);
-  var mins = ~~((duration % 3600) / 60);
+  // var hrs = ~~(duration / 3600);
+  var mins = ~~(duration / 60);
   var secs = ~~duration % 60;
 
   // Output like "1:01" or "4:03:59" or "123:03:59"
   var ret = '';
 
-  if (hrs > 0) {
-    ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
-  }
+  // if (hrs > 0) {
+  //   ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
+  // }
 
   ret += '' + mins + ':' + (secs < 10 ? '0' : '');
   ret += '' + secs;
